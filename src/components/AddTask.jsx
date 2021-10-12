8376a089c667d9ba17a092a23807b6285f5eb34c
Alisson Cipriano <58789035+AlissonCiprian0@users.noreply.github.com>
2021-10-12 17:38:55 -0300

    confirmar em 'master'

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+// CSS
+import "./AddTask.css";
+
+const AddTask = ({ handleTaskAddition }) => {
+    const [inputData, setInputData] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    }
+
+    const checkEmptyInput = () => {
+        if (inputData && inputData != "" && inputData.trim().length > 0)
+            return true;
+        return false;
+    }
+
+    const handleAddTaskClick = () => {
+        if (checkEmptyInput()) {
+            handleTaskAddition(inputData);
+            setInputData('');
+        }
+        else {
+            alert('Preencha o input!');
+        }
+    }
+
+    return (
+        <div className="add-task-container">
+            <input onChange={handleInputChange} value={inputData} type="text" className="add-task-input" placeholder="Título da tarefa" />
+
+            <div className="add-task-button-container">
+                <Button onClick={handleAddTaskClick}>Adicionar</Button>
+            </div>
+        </div>
+    );
+}
+ 
+export default AddTask;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Task from './Task';
+
+const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
+    return (
+        <>
+            {
+                tasks.map(task => (
+                    <Task key={task.id} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion} task={task} />
+                ))
+            }
+        </>
+    );
+}
+
+export default Tasks;
